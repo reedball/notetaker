39ec8169f9d4254026037030cfaa1e31a0ef2110
Reed Crawley <rcrawls@yahoo.com>
2021-03-08 15:33:54 -0500

    test

---
+++ routes/apiRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const store = require('../db/store');
+
+
+router.get('/notes', (req, res) => {
+    store.getNotes().then(notes => res.json(notes));
+})
+
+router.post('/notes', (req, res) => {
+    store.addNote(req.body).then(note => res.json(note));
+    
+})
+
+router.delete('/notes/:id', (req, res) => {
+    store.removeNote(req.params.id).then(() => res.json({ok: true}));
+})
+
+module.exports = router;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+
+const router = express.Router();
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+module.exports = router;
